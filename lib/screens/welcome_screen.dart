443c707f0d3a260ac4a7f0091eb1e7a9672ad507
Rohit Singh <conankmails@gmail.com>
2020-12-21 02:51:56 +0530

    Screen Routes created

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatefulWidget {
+
+  static String id = 'welcome_screen';
+
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
