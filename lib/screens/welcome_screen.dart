016c59951705075dc56af75252d89190b74e8410
Rohit Singh <conankmails@gmail.com>
2020-12-24 04:35:22 +0530

    more animations added

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -10,11 +10,46 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin{
+  AnimationController controller;
+  Animation animation;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      duration: Duration(seconds: 3),
+      vsync: this,
+    );
+
+    //for curved animations
+   //animation = CurvedAnimation(parent: controller,curve:Curves.decelerate );
+
+    //for tweek animations
+    animation = ColorTween(begin: Colors.blueGrey,end: Colors.white).animate(controller);
+
+    controller.forward();
+
+    controller.addListener(() {
+      setState(() {
+
+      });
+      print(controller.value);
+    });
+  }
+
+
+@override
+  void dispose() {
+    // TODO: implement dispose
+  controller.dispose();
+    super.dispose();
+  }
+
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -31,7 +66,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   ),
                 ),
                 Text(
-                  'Flash Chat',
+                  'Just Chat',
                   style: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
