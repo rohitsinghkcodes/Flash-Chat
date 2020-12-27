575639d7a657b3a4ce9388f051410d7d2dcf277d
Rohit Singh <conankmails@gmail.com>
2020-12-28 03:45:55 +0530

    chat UI feature completed

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -5,6 +5,9 @@ import 'package:flash_chat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+final _fireStore = FirebaseFirestore.instance;
+User loggedInUser;
+
 class ChatScreen extends StatefulWidget {
   static const String id = 'chat_screen';
 
@@ -13,9 +16,11 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  //for clearing the typed msg after sending
+  final messageTextController = TextEditingController();
+
   final _auth = FirebaseAuth.instance;
-  final _fireStore = FirebaseFirestore.instance;
-  User loggedInUser;
+
   String msgText;
 
   @override
@@ -50,7 +55,6 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
         leading: null,
         actions: <Widget>[
           IconButton(
@@ -61,7 +65,6 @@ class _ChatScreenState extends State<ChatScreen> {
                 Navigator.pop(context);
               }),
         ],
-
         title: Center(
           child: Text(
             '⚡️Chat',
@@ -74,35 +77,7 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            StreamBuilder<QuerySnapshot>(
-              stream: _fireStore.collection('messages').snapshots(),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return Center(
-                    child: CircularProgressIndicator(
-                      backgroundColor: Colors.blue[900],
-                    ),
-                  );
-                }
-                final messages = snapshot.data.docs;
-                List<MsgBubble> messageBubbles = [];
-                for (var msg in messages) {
-                  final messageText = msg.data()['text'];
-                  final messageSender = msg.data()['sender'];
-                  final messageBubble =
-                      MsgBubble(text: messageText, sender: messageSender);
-
-                  messageBubbles.add(messageBubble);
-                }
-                return Expanded(
-                  child: ListView(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-                    children: messageBubbles,
-                  ),
-                );
-              },
-            ),
+            MessageStream(),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -110,6 +85,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: messageTextController,
                       onChanged: (value) {
                         //Do something with the user input.
                         msgText = value;
@@ -119,11 +95,17 @@ class _ChatScreenState extends State<ChatScreen> {
                   ),
                   FlatButton(
                     onPressed: () {
-                      //Implement send functionality.
-                      _fireStore.collection('messages').add({
-                        'text': msgText,
-                        'sender': loggedInUser.email,
-                      });
+                      //for clearing the text field
+                      if(msgText!=null)
+                        {
+                          //Implement send functionality.
+                          _fireStore.collection('messages').add({
+                            'text': msgText,
+                            'sender': loggedInUser.email,
+                          });
+                        }
+                      messageTextController.clear();
+                      msgText=null;
                     },
                     child: Text(
                       'Send',
@@ -140,31 +122,85 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 }
 
+class MessageStream extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _fireStore.collection('messages').snapshots(),
+      
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.blue[900],
+            ),
+          );
+        }
+        final messages = snapshot.data.docs;
+        List<MsgBubble> messageBubbles = [];
+        for (var msg in messages) {
+          final messageText = msg.data()['text'];
+          final messageSender = msg.data()['sender'];
+
+          final currUser = loggedInUser.email;
+
+          final messageBubble = MsgBubble(
+            text: messageText,
+            sender: messageSender,
+            isCurUser: currUser == messageSender,
+          );
+
+          messageBubbles.add(messageBubble);
+        }
+        return Expanded(
+          child: ListView(
+            reverse: true,
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+            children: messageBubbles,
+          ),
+        );
+      },
+    );
+  }
+}
+
 class MsgBubble extends StatelessWidget {
-  MsgBubble({@required this.text, @required this.sender});
+  MsgBubble(
+      {@required this.text, @required this.sender, @required this.isCurUser});
   final String text;
   final String sender;
+  final bool isCurUser;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(10.0),
       child: Column(
+        crossAxisAlignment: isCurUser ? CrossAxisAlignment.end : CrossAxisAlignment.start,
         children: [
-          Text(sender,
-          style: TextStyle(
-            color: Colors.grey[800],
-            fontSize: 12.0,
-          ),),
+          Text(
+            sender,
+            style: TextStyle(
+              color: Colors.grey[600],
+              fontSize: 12.0,
+            ),
+
+          ),
+          SizedBox(height: 2.0,),
           Material(
             elevation: 5.0,
-            borderRadius: BorderRadius.circular(30.0),
-            color: Colors.blueAccent[400],
+            borderRadius: isCurUser ? BorderRadius.only(
+                topLeft: Radius.circular(30),
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30)) : BorderRadius.only(
+                topRight: Radius.circular(30),
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30)),
+            color: isCurUser ? Colors.blueAccent[700] : Colors.grey[850],
             child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-              child: Text(text,
-                style: TextStyle(fontSize: 15,
-                color: Colors.white),
-
+              padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 20.0),
+              child: Text(
+                text,
+                style: TextStyle(fontSize: 15, color: Colors.white),
               ),
             ),
           ),
